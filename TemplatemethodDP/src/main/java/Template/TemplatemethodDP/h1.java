533df7dd78436bebe8b4b4f11346a2ac11bf21d3
Konda-Akhila <akhilaappu68@gmail.com>
2020-07-21 18:41:11 +0530

    Sixth commit

---
+++ TemplatemethodDP/src/main/java/Template/TemplatemethodDP/h1.java
@@ -0,0 +1,15 @@
+package Template.TemplatemethodDP;
+
+public class h1 extends h2 {
+
+	@Override
+	public void buildWalls() {
+		System.out.println("Building Glass Walls");
+	}
+
+	@Override
+	public void buildPillars() {
+		System.out.println("Building Pillars with glass coating");
+	}
+
+}
---
+++ TemplatemethodDP/src/main/java/Template/TemplatemethodDP/h3.java
@@ -0,0 +1,15 @@
+package Template.TemplatemethodDP;
+
+public class h3 extends h2 {
+
+	@Override
+	public void buildWalls() {
+		System.out.println("Building Wooden Walls");
+	}
+
+	@Override
+	public void buildPillars() {
+		System.out.println("Building Pillars with Wood coating");
+	}
+
+}
