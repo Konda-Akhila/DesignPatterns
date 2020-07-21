bf509d5d489bd8aabc5ff934dcd2ad0882e14b1b
Konda-Akhila <akhilaappu68@gmail.com>
2020-07-21 18:38:34 +0530

    Fourth commit

---
+++ PrototypeDP/src/main/java/Proto/PrototypeDP/Employees.java
@@ -0,0 +1,39 @@
+package Proto.PrototypeDP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employees implements Cloneable{
+
+	private List<String> empList;
+	
+	public Employees(){
+		empList = new ArrayList<String>();
+	}
+	
+	public Employees(List<String> list){
+		this.empList=list;
+	}
+	public void loadData(){
+		//read all employees from database and put into the list
+		empList.add("aravind");
+		empList.add("hasin");
+		empList.add("ravi");
+		empList.add("raju");
+	}
+	
+	public List<String> getEmpList() {
+		return empList;
+	}
+
+	@Override
+	public Object clone() throws CloneNotSupportedException{
+			List<String> temp = new ArrayList<String>();
+			for(String s : this.getEmpList()){
+				temp.add(s);
+			}
+			return new Employees(temp);
+	}
+
+	
+}
